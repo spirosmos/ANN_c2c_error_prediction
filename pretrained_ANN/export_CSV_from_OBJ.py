80cb0c5655571d4f2e8a93ce0eb5fd31477a1bd4
spirosmos <spirosmos@gmail.com>
2022-06-13 12:45:49 +0300

    Update export_CSV_from_OBJ.py

--- pretrained_ANN/export_CSV_from_OBJ.py
+++ pretrained_ANN/export_CSV_from_OBJ.py
@@ -4,6 +4,7 @@ import numpy as np
 import igl
 import math
 from csv import writer,reader
+import sys
 
 def append_list_as_row(file_name, list_of_elem): 
     with open(file_name, 'a+', newline='') as write_obj:
